6deb211d726dd27bd9b17ee855658d2c3bb8afbb
Joe <joehdodd@gmail.com>
2018-01-26 16:00:59 -0500

    created storeCard mixin and stores template, successfully useing mongo .find() method to return array of stores and render cards

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -1,16 +1,17 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 
-exports.homePage = (req, res) => {
-  res.render('index');
-};
-
 exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store'});
 };
 
 exports.createStore = async (req, res) => {
-  const store = new Store(req.body);
-  await store.save();
-  res.redirect('/');
+  const store = await (new Store(req.body)).save();
+  req.flash('success', `Sucessfully created ${store.name}! Care to leave a review?`);
+  res.redirect(`/store/${store.slug}`);
 };
+
+exports.getStores = async (req, res) => {
+  const stores = await Store.find();
+  res.render('stores', { title: 'Stores', stores });
+}
