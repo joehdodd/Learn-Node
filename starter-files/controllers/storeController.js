1072249fb8bd200ddfbff1d399dbc13751e8cad6
Joe Dodd <joehdodd@gmail.com>
2018-06-11 14:57:56 -0400

    edit or update stores functionality

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -16,3 +16,16 @@ exports.getStores = async (req, res) => {
   const stores = await Store.find();
   res.render('stores', { title: 'Stores', stores });
 }
+exports.editStore = async (req, res) => {
+  const store = await Store.findOne({ _id: req.params.id });
+  console.log(store);
+  res.render('editStore', { title: 'Edit Store', store })
+}
+exports.updateStore = async (req, res) => {
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, // return the new store, instead of old
+    runValidators: true //
+  }).exec();
+  req.flash('success', `Successfully update ${store.name} <a href="/stores/${store.slug}">View</a>`)
+  res.redirect(`/stores/${store._id}/edit`)
+}
